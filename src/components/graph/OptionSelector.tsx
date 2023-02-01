f279791ff4ba94c175ab759b5d8ab636ab566acb
jintak0401 <jintak0401@naver.com>
2023-02-01 13:13:51 +0900

    feat: 테이블 옵션을 설정할 수 있는 컴포넌트 구현

---
+++ src/components/graph/OptionSelector.tsx
@@ -0,0 +1,97 @@
+interface Props {
+  type: string;
+  options: any;
+  setOptions: (option: any) => void;
+}
+
+const assignOption = (options: any, path: string[], value: any) => {
+  const originOptions = options;
+  for (let i = 0; i < path.length - 1; i++) {
+    options[path[i]] ??= {};
+    options = options[path[i]];
+  }
+  options[path[path.length - 1]] = value;
+  return originOptions;
+};
+
+const chainOption = (options: any, path: string[]) => {
+  for (let i = 0; i < path.length - 1; i++) {
+    if (options[path[i]] === undefined) {
+      return 0;
+    }
+    options = options[path[i]];
+  }
+  return options[path[path.length - 1]];
+};
+
+const COMMON_OPTIONS = [
+  {
+    label: 'Label Font Size',
+    path: ['plugins', 'datalabels', 'font', 'size'],
+  },
+  {
+    label: 'Legend Font Size',
+    path: ['plugins', 'legend', 'labels', 'font', 'size'],
+  },
+];
+
+const PIE_OPTIONS = [...COMMON_OPTIONS];
+
+const XY_CHART_OPTIONS = [
+  ...COMMON_OPTIONS,
+  {
+    label: 'X Axis Font Size',
+    path: ['scales', 'x', 'ticks', 'font', 'size'],
+  },
+  {
+    label: 'Y Axis Font Size',
+    path: ['scales', 'y', 'ticks', 'font', 'size'],
+  },
+];
+
+const RADAR_OPTIONS = [
+  ...COMMON_OPTIONS,
+  {
+    label: 'Point Label Font Size',
+    path: ['scales', 'r', 'pointLabels', 'font', 'size'],
+  },
+];
+
+const OPTIONS_MAP = {
+  pie: PIE_OPTIONS,
+  line: XY_CHART_OPTIONS,
+  bar: XY_CHART_OPTIONS,
+  radar: RADAR_OPTIONS,
+};
+
+const OptionSelector = ({ type, options, setOptions }: Props) => {
+  const optionsList = OPTIONS_MAP[type as keyof typeof OPTIONS_MAP];
+  return (
+    <div className="flex gap-2">
+      {optionsList.map((option) => (
+        <div key={option.label}>
+          <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+            {option.label}
+          </label>
+          <input
+            type="number"
+            className="block w-full rounded-md border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500"
+            min={0}
+            value={chainOption(options, option.path) || 0}
+            onChange={(e) =>
+              setOptions(
+                assignOption(
+                  { ...options },
+                  option.path,
+                  Number(e.target.value)
+                )
+              )
+            }
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default OptionSelector;
