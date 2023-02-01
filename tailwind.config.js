6638dfa2d9f4f599604c5bda62e655ed2008a2e3
jintak0401 <jintak0401@naver.com>
2023-02-01 13:08:59 +0900

    chore: 라이브러리 설치

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.indigo,
+        gray: colors.neutral,
+      },
+    },
+  },
+  plugins: [],
+};
