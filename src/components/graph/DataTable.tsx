dd981f4f51991432b86ca1de02aa0ee5a3d42457
jintak0401 <jintak0401@naver.com>
2023-02-01 13:14:10 +0900

    feat: 차트 데이터를 설정할 수 있는 컴포넌트 구현

---
+++ src/components/graph/DataTable.tsx
@@ -0,0 +1,189 @@
+import { useState } from 'react';
+import { CompactPicker } from 'react-color';
+
+interface Props {
+  type: string;
+  row: number;
+  setRow: (row: number) => void;
+  col: number;
+  setCol: (col: number) => void;
+  data: number[][];
+  setData: (data: number[][]) => void;
+  labels: string[];
+  setLabels: (labels: string[]) => void;
+  items: string[];
+  setItems: (items: string[]) => void;
+  colors: string[];
+  setColors: (colors: string[]) => void;
+}
+
+interface CellProps {
+  type: string;
+  value: string | number;
+  setText: (text: string) => void;
+  color?: string;
+  setColor?: (color: string) => void;
+}
+
+const InputCell = ({ type, value, setText, color, setColor }: CellProps) => {
+  const [opened, setOpened] = useState(false);
+  return (
+    <div className="relative flex">
+      <input
+        type={type}
+        className="block w-full border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500"
+        value={value || 0}
+        required
+        onChange={(e) => setText(e.target.value)}
+      />
+      {color && setColor && (
+        <>
+          <button
+            onClick={() => setOpened((prev) => !prev)}
+            className="h-10 w-10"
+            style={{ backgroundColor: color }}
+          />
+          {opened && (
+            <div className="absolute top-full z-10">
+              <div
+                className="fixed right-0 top-0 left-0 bottom-0"
+                onClick={() => setOpened(false)}
+              />
+              <CompactPicker
+                color={color}
+                onChange={(color) => {
+                  setColor(color.hex);
+                }}
+              />
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+const RowColSetter = ({ type, row, col, setRow, setCol }: Props) => {
+  return (
+    <div className="flex gap-2">
+      {['row', 'col'].map((label) => (
+        <div key={label}>
+          <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+            {label}
+          </label>
+          <input
+            type="number"
+            id={label}
+            className={`block w-full rounded-md border border-gray-300 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-blue-500 ${
+              label === 'row' && type === 'pie' ? 'bg-gray-200' : 'bg-gray-50'
+            }`}
+            value={label === 'row' ? row : col}
+            onChange={(e) => {
+              if (label === 'row') {
+                setRow(Number(e.target.value));
+              } else {
+                setCol(Number(e.target.value));
+              }
+            }}
+            required
+            disabled={label === 'row' && type === 'pie'}
+            min={1}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const Table = ({
+  labels,
+  setLabels,
+  items,
+  setItems,
+  setData,
+  data,
+  type,
+  colors,
+  setColors,
+  row,
+  col,
+}: Props) => {
+  const setLabelWithIdx = (idx: number) => (text: string) => {
+    const newLabels = [...labels];
+    newLabels[idx] = text;
+    setLabels(newLabels);
+  };
+
+  const setItemWithIdx = (idx: number) => (text: string) => {
+    const newItems = [...items];
+    newItems[idx] = text;
+    setItems(newItems);
+  };
+
+  const setDataWithIdx = (i: number, j: number) => (text: string) => {
+    const newData = [...data];
+    newData[i][j] = Number(text);
+    setData(newData);
+  };
+
+  const setColorWithIdx = (idx: number) => (color: string) => {
+    const newColors = [...colors];
+    newColors[idx] = color;
+    setColors(newColors);
+  };
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th />
+          {labels.map((label, i) => (
+            <th key={i}>
+              <InputCell
+                value={label}
+                color={type === 'pie' ? colors[i] : undefined}
+                setColor={type === 'pie' ? setColorWithIdx(i) : undefined}
+                type="text"
+                setText={setLabelWithIdx(i)}
+              />
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {Array.from({ length: row }, (_, i) => i).map((i) => (
+          <tr key={`row_${i}`}>
+            <td>
+              <InputCell
+                type="text"
+                color={type !== 'pie' ? colors[i] : undefined}
+                setColor={type !== 'pie' ? setColorWithIdx(i) : undefined}
+                value={items[i]}
+                setText={setItemWithIdx(i)}
+              />
+            </td>
+            {Array.from({ length: col }, (_, j) => j).map((j) => (
+              <td key={`col_${j}`}>
+                <InputCell
+                  type="number"
+                  value={data[i]?.[j] || 0}
+                  setText={setDataWithIdx(i, j)}
+                />
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const DataTable = (props: Props) => {
+  return (
+    <div className="space-y-2">
+      <RowColSetter {...props} />
+      <Table {...props} />
+    </div>
+  );
+};
+
+export default DataTable;
