57a33cd4afae520bb01dca484032cdb68faf1f08
jintak0401 <jintak0401@naver.com>
2023-02-04 12:07:22 +0900

    feat: 헤더에 로고 추가 (#13)

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,8 @@
 import metadata from 'data/metadata';
 import { Dropdown } from 'flowbite-react';
 import { useAtom } from 'jotai';
+import NextImage from 'next/image';
+import Link from 'next/link';
 import { HiMenuAlt1 } from 'react-icons/hi';
 
 import EN from '@/assets/country/en.svg';
@@ -24,16 +26,28 @@ const Header = () => {
 
   return (
     <div className="fixed top-0 left-0 z-50 flex h-14 w-full items-center justify-between border-b bg-white px-4">
-      <button
-        className="btn-ghost btn-square btn-sm btn"
-        onClick={() =>
-          setDrawerState({
-            drawerOpened: !drawerState.drawerOpened,
-          })
-        }
-      >
-        <HiMenuAlt1 className="h-6 w-6" />
-      </button>
+      <div className="flex items-center gap-4">
+        <button
+          className="btn-ghost btn-square btn-sm btn"
+          onClick={() =>
+            setDrawerState({
+              drawerOpened: !drawerState.drawerOpened,
+            })
+          }
+        >
+          <HiMenuAlt1 className="h-6 w-6" />
+        </button>
+        <Link className="flex items-center gap-2 text-xl font-bold" href="/">
+          <NextImage
+            className="h-10 w-10 object-contain"
+            src="/static/images/simryun.png"
+            alt="logo"
+            width="200"
+            height="200"
+          />
+          SIMRYUN
+        </Link>
+      </div>
       <Dropdown
         arrowIcon={false}
         inline={true}
