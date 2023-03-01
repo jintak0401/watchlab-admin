f1bb2246061b6f4521f8b0def29de832ef5853b3
jintak0401 <jintak0401@naver.com>
2023-03-01 22:03:56 +0900

    feat: 저장된 locale과 다를 때에만 새 locale을 저장하도록 변경 (#21)

--- src/hooks/useStoreLocale.ts
+++ src/hooks/useStoreLocale.ts
@@ -1,12 +1,15 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
-import { NO_NEED_STORE_LOCALE_URLS } from '@/lib/constant';
+import { LOCALE_KEY, NO_NEED_STORE_LOCALE_URLS } from '@/lib/constant';
 
 const useStoreLocale = () => {
   const router = useRouter();
   useEffect(() => {
-    if (NO_NEED_STORE_LOCALE_URLS.includes(router.pathname)) {
+    if (
+      NO_NEED_STORE_LOCALE_URLS.includes(router.pathname) ||
+      router.locale === localStorage.getItem(LOCALE_KEY)
+    ) {
       return;
     }
     localStorage.setItem('locale', router.locale as string);
