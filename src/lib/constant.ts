f06f404b875d1a2463a882b0d6fc33faa05bc403
jintak0401 <jintak0401@naver.com>
2023-03-03 23:28:12 +0900

    feat: dictionary, gallery 관련 api 키값 별도 분리 및 허용 이미지 확장자 추가 (#19)

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -6,3 +6,13 @@ export const NO_NEED_STORE_LOCALE_URLS = ['/', '/auth'];
 export const LOCALE_KEY = 'locale';
 
 export const NO_NEED_LOGIN_URLS = ['/', '/auth'];
+
+export const VALID_IMAGE_TYPES = [
+  'image/webp',
+  'image/png',
+  'image/jpeg',
+  'image/gif',
+];
+
+export const DICTIONARY_KEY = 'dictionary';
+export const GALLERY_KEY = 'gallery';
