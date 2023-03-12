b3c46c47adb10fa2e7c6603f608c6afbf142eccc
jintak0401 <jintak0401@naver.com>
2023-03-12 16:17:51 +0900

    feat: 포스트 삭제 모달창 구현 (#37)

---
+++ src/components/Posts/List/ModalDelete.tsx
@@ -0,0 +1,44 @@
+import { Button, Modal } from 'flowbite-react';
+import { useState } from 'react';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+
+interface Props {
+  opened: boolean;
+  onClose: () => void;
+  onConfirm: () => Promise<void> | void;
+}
+
+const ModalDelete = ({ opened, onClose, onConfirm }: Props) => {
+  const [loading, setLoading] = useState(false);
+  const confirm = () => {
+    setLoading(true);
+    onConfirm();
+  };
+  return (
+    <Modal show={opened} size="md" popup={true} onClose={onClose}>
+      <Modal.Header />
+      <Modal.Body>
+        <div className="text-center">
+          <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+          <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+            Are you sure you want to delete this post?
+          </h3>
+          <div className="flex justify-center gap-4">
+            <Button
+              color={loading ? 'gray' : 'failure'}
+              onClick={confirm}
+              disabled={loading}
+            >
+              {loading ? 'Loading...' : "Yes, I'm sure"}
+            </Button>
+            <Button color="gray" onClick={onClose}>
+              No, cancel
+            </Button>
+          </div>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default ModalDelete;
